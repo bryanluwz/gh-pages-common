5b5045c165cddca5e62013c0006dd478b5215762
Bryan <bryanlu.my@gmail.com>
2023-07-21 14:31:40 +0800

    updated progress bar

--- others/index.js
+++ others/index.js
@@ -2,3 +2,4 @@ export { Button, DangerousButton, AmnesiaButton } from "./Button";
 export { default as ContentDisplay } from "./ContentDisplay";
 export { default as Searchbar } from "./Searchbar";
 export { default as StickySidebar } from "./StickySidebar";
+export { default as ProgressBar } from "./ProgressBar";
