c2a3379db3ffb0486f7ef33d69ac94aad9cf569d
Bryan <bryanlu.my@gmail.com>
2023-07-21 14:20:16 +0800

    added progress bar

---
+++ others/ProgressBar.js
@@ -0,0 +1,30 @@
+import { Component, createRef } from "react";
+import "./ProgressBar.css";
+
+export default class ProgressBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			progress: this.props.progress ? this.props.progress : 0
+		};
+
+		this.barRef = createRef();
+	}
+
+	setProgress = (progress) => {
+		this.setState({ progress: progress }, () => {
+			this.barRef.current.style.width = `${progress * 100}%`;
+		});
+	};
+
+	render() {
+		return (
+			<div className={`progress-bar-wrapper${this.props.wrapperClass ? this.props.wrapperClass : ""}`}>
+				<div className={`progress-bar-container ${this.props.containerClass ? this.props.containerClass : ""}`}>
+					<div ref={this.barRef} className={`progress-bar ${this.props.barClass ? this.props.barClass : ""}`}></div>
+				</div>
+			</div>
+		);
+	}
+}
